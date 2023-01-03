3124f648bc6dfee849ac2414ea2f1d04907861be
arshdeepkumar482 <113937019+arshdeepkumar482@users.noreply.github.com>
2023-01-03 17:14:01 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("inside action");
+
+const fs = require('fs')
+fs.readdir(__dirname, (err, files) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("\nCurrent directory filenames:");
+    files.forEach(file => {
+      console.log(file);
+    })
+  }
+})
+  
